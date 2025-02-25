5e2f91c43ef21205a3b1114affb23ddaa0bb3f16
Amit Yadav <amit09yadav12@gmail.com>
2025-02-26 03:39:41 +0530

    minor

--- redux-toolkit/src/components/AddTodo.jsx
+++ redux-toolkit/src/components/AddTodo.jsx
@@ -25,7 +25,7 @@ return(
         type="submit"
         className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
       >
-        Add Todo
+        ADD TODO
       </button>
     </form>
 )
